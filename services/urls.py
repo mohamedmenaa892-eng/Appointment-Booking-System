2729bb156945bd293c2db233d4c1d1dfda7b8bec
mohamedmenaa892-eng <mohamedmenaa892@gmail.com>
2026-04-13 19:29:38 +0100

    add the health path

--- services/urls.py
+++ services/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.services_view,name='services_view')
+    path('',views.services_view,name='services_view'),
+    path('health/',views.health,name='health')
 ]
--- services/views.py
+++ services/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest , HttpResponse
 from .models import Services
 
 
 def services_view(request:HttpRequest):
     services = Services.objects.all()
     return render(request,'service/home.html',{'services':services})
+
+
+def health(request:HttpRequest):
+    return HttpResponse('I am alive')
